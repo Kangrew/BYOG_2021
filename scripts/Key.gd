2186c4a05b543667b23d7a98ffbe2c06e246b60b
Jerrin Thomas <jd0christo1678@gmail.com>
2021-10-23 17:42:56 +0530

    Added Lock and Key Mech

---
+++ scripts/Key.gd
@@ -0,0 +1,47 @@
+extends Area2D
+onready var keySprite     = $Sprite;
+onready var tween   = $Tween;
+
+export var anim_duration : float = 0.5;
+export var initialScale : float = 1;
+export var finalScale : float = 0.7;
+
+export var locks = [];
+
+var isScalingUp : bool = false;
+
+func _ready():
+	scalingAnimation(initialScale, finalScale, anim_duration);
+	pass
+
+func _on_Area2D_area_shape_entered(area_id, area, area_shape, local_shape):
+	if area.is_in_group("Player"):
+		tween.stop_all();
+		keySprite.scale = Vector2(0.7,0.7);
+		unlockAllLock();
+
+func _on_Key_area_shape_exited(area_id, area, area_shape, local_shape):
+	if area.is_in_group("Player"):
+		scalingAnimation(initialScale, finalScale, anim_duration);
+		isScalingUp = true;
+	pass
+
+func _on_Tween_tween_completed(object, key):
+	if isScalingUp == false:
+		scalingAnimation(initialScale, finalScale, anim_duration);
+		isScalingUp = true;
+	else:
+		scalingAnimation(finalScale, initialScale, anim_duration);
+		isScalingUp = false;
+
+func unlockAllLock():
+	for i in locks.size():
+		var lock : Lock = get_node(locks[i]);
+		lock.unlock();
+	pass
+
+func scalingAnimation(scaleDown, scaleUp, seconds):
+	tween.interpolate_property(keySprite, "scale", Vector2(scaleDown,scaleDown), Vector2(scaleUp,scaleUp), seconds, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT);
+	tween.start()
+	
+	pass
---
+++ scripts/Lock.gd
@@ -0,0 +1,47 @@
+extends Area2D
+class_name Lock
+
+onready var lockSprite = $Sprite;
+onready var tween   = $Tween;
+onready var collisionShape : CollisionShape2D = $CollisionShape2D;
+
+export var anim_duration : float = 0.5;
+export var lockColor : Color = Color.white;
+export var unlockColor : Color = Color.white;
+
+var islocked : bool = false;
+
+func _ready():
+	lock();
+	pass # Replace with function body.
+
+func unlock():
+	if(!islocked):
+		return;
+	
+	islocked = false;
+	collisionShape.set_deferred("disabled", true);
+	switchColor(lockColor, unlockColor, anim_duration);
+	pass
+
+func lock():
+	if(islocked):
+		return;
+	
+	islocked = true;
+	collisionShape.set_deferred("disabled", false);
+	switchColor(unlockColor,lockColor,anim_duration);
+	pass
+
+func switchColor(aColor, bColor, seconds):
+	tween.interpolate_property(lockSprite, "self_modulate",aColor, bColor, seconds, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT);
+	tween.start()
+	pass
+
+
+func _on_Lock_area_shape_entered(area_id, area, area_shape, local_shape):
+	if area.is_in_group("Player") and islocked:
+		var player : Player = area.get_parent();
+		player.die();
+		pass
+	pass # Replace with function body.
